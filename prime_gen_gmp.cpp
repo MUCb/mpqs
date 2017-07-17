01725fd23f5978009b44ad045a7d72f289a3dff8
Vitalii Misko <vitalik560@yandex.ru>
2017-07-16 21:45:08 -0700

    add primes

--- prime_gen_gmp.cpp
+++ prime_gen_gmp.cpp
@@ -7,89 +7,105 @@
 // #define PRIMES_SIZE 999999
 
 int main (){
-
 	// const unsigned int sumX = 999999;
 	// mpz_t primes[sumX];
 	mpz_t prime;
 	// mpz_class A[sumX];
 	// primes = (mpz_t *) malloc(sizeof(mpz_t) * PRIMES_SIZE);
+
 	mpz_t m;
 	mpz_init(prime);
+	mpz_init(m);
 	// mpz_init_set_ui(primes[0], 2);
 	// mpz_init_set_ui(primes[1], 3);
 
 	char str[60];
 	// int prime_current = 2;
 
-	mpz_set_str (m, "999999999999999", 10);
+	// printf("%s %d\n", __func__, __LINE__);
 
 	// gmp_printf ("%Zd,\n",primes[0]);
 	// gmp_printf ("%Zd,\n",primes[1]);
-	FILE * fd = fopen("primes.txt", "w+");
+	char file_name[30];
+	int exp = 9;
+	// mpz_set_str (m, "999999999999999", 10);
+	mpz_ui_pow_ui(m,10, exp);
+	sprintf(file_name, "primes/10x%d.txt", exp);
+	// printf("%s %d\n", __func__, __LINE__);
+
+	FILE * fd = fopen(file_name, "w+");
 	if(fd == NULL) 
    {
       perror("Error opening file");
       return(-1);
    }
 
-	fputs("2\n", fd);
-	fputs("3\n", fd);
+	// fputs("2\n", fd);
+	// fputs("3\n", fd);
 	// return 0;
+
 	mpz_t i;
-	for (mpz_init_set_ui(i, 4); mpz_cmp(i,m) < 0; mpz_add_ui(i, i, 1))
+	mpz_init(i);
+	// mpz_set_ui(i, 10);
+	
+	printf("%s %d\n", __func__, __LINE__);
+	for (mpz_ui_pow_ui(i,10, exp-1) ; mpz_cmp(i,m) < 0; mpz_add_ui(i, i, 1))
 	// // // for (mpz_init_set_ui(i, 4); mpz_cmp(i,m) < 0; mpz_add_ui(i, i, 1))
     {
-    	// seek(fd, 0, SEEK_END);
-    	fseek(fd, 0, SEEK_SET);
-		bool not_prime = false;
-		mpz_t sqrt;
-		mpz_init(sqrt);	
-    	mpz_sqrt (sqrt, i);
-    	mpz_add_ui(sqrt, sqrt, 1);
-    	int j;
+    	mpz_t i_sqrt;
+		mpz_init(i_sqrt);
+    	mpz_sqrt(i_sqrt, i);
+    	mpz_add_ui(i_sqrt, i_sqrt, 1);
+    	bool not_prime = false;
+    	bool finish = false;
 
-		// gmp_printf ("%Zd,\n",i);
-    	// for (j = 0; mpz_cmp(primes[j],sqrt) < 0 && j < prime_current; j++)
-	    // {
-		size_t red_b;
-	    	// while( (red_b = mpz_inp_str (prime , fd, 60 )) && red_b != 0){
-	    	while( fgets (str , 60 , fd) != NULL){
-	    		// printf("%s == \n", str);
-				mpz_set_str (prime, str, 10);
-		    	// fseek(fd, red_b, SEEK_SET);
-		   		if(mpz_divisible_p(i, prime) != 0){
+		for (int j = 1; j < exp; ++j)
+		{
+			char fn[30];
+			sprintf(fn, "primes/10x%d.txt", j);
+			FILE * fd_t = fopen(fn, "r");
+			if(fd_t == NULL) 
+			{
+		      	perror("Error opening file");
+		      	return(-1);
+		    }
+
+		    while( fgets (str , 60 , fd_t) != NULL){
+		    	mpz_set_str (prime, str, 10);
+		    	if(mpz_divisible_p(i, prime) != 0){
 						// gmp_printf ("not prime ==== %Zd\n",i);
 		    			not_prime = true;
 		    			break;
 		    	}
 
-	    	}
-		    	if(not_prime == false){
-					gmp_printf ("prime %Zd,\n",prime);
-		    		// mpz_set(primes[prime_current], i);
-			    	fseek(fd, 0, SEEK_END);
-			    	// break;
-		    		mpz_out_str(fd, 10, i);
-					// prime_current++;
-					fputs("\n", fd);
-					// return 0;
+		    	if (mpz_cmp(prime,i_sqrt) > 0)
+		    	{
+		    		finish = true;
+		    		break;
 		    	}
 
+		    }
 
-		// 		// gmp_printf ("check  %Zd\n",primes[j]);
-	 
-	 //    	}
+			fclose(fd_t);
 
-	 //    	// if(prime_current >= PRIMES_SIZE)
-	 //    	// {
-	 //    	// 	printf("ERROR to samll array\n");
-	 //    	// 	return 0;
-	 //    	// }
-    		
-    	// }
-		// gmp_printf ("swrt %Zd i %Zd\n",sqrt, i);
+			if(finish == true)
+				break;
 
-		
+		    if(not_prime == true)
+		    	break;
+		}
+
+	    if(not_prime == false){
+			gmp_printf ("prime %Zd,\n",i);
+    		// mpz_set(primes[prime_current], i);
+	    	// fseek(fd, 0, SEEK_END);
+	    	// break;
+    		mpz_out_str(fd, 10, i);
+			// prime_current++;
+			fputs("\n", fd);
+					// return 0;
+		}
+
 	}
 	fclose(fd);
 
